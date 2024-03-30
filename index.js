ef404a635726e52ce1d8944eb46d3afd3d6af8ad
Ram <ram440163@gmail.com>
2024-03-31 00:52:26 +0530

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById('loginButton');
+
+loginButton.addEventListener('click', ()=>{
+       window.location.href = "http://127.0.0.1:3000/index.Formtable.html";
+
+})
